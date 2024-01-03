1e61d6913ce9e8f2b6bfbe688dd3480a707b594f
Salman Ahmed <isalmandev@gmail.com>
2024-01-03 06:12:39 +0500

    feat: initial setup

---
+++ src/utils/constants.ts
@@ -0,0 +1,27 @@
+export const NAVBAR = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+];
+
+export const SOCIALS = [
+  {
+    label: "linkedin",
+    href: "https://www.linkedin.com/in/salmangujjar-dev",
+  },
+  {
+    label: "github",
+    href: "https://www.github.com/salmangujjar-dev",
+  },
+];
+
+export const EMAIL = "mailto:isalmandev@gmail.com";
