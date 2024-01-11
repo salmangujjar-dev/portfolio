26babffe020f111d3f943a8d9f03da610c90e187
Salman Ahmed <isalmandev@gmail.com>
2024-01-11 22:53:46 +0500

    chore: finalized header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { Variants, motion } from "framer-motion";
+import { AnimatePresence, Variants, motion } from "framer-motion";
 
 import { EScreenType, useMediaQuery } from "../hooks/useMediaQuery";
+import useMounted from "../hooks/useMounted";
 
 import { NAVBAR_OPTIONS } from "@utils/constants";
 import { clsxm } from "@utils/clsxm";
@@ -17,32 +18,37 @@ const variants: Variants = {
     opacity: 1,
     display: "flex",
     pointerEvents: "none",
-    transitionEnd: {
-      pointerEvents: "auto",
-    },
     transition: {
       y: { stiffness: 1000, velocity: -100 },
+    },
+    transitionEnd: {
+      pointerEvents: "auto",
     },
   },
   closed: {
     y: "-100%",
-    transitionEnd: {
-      display: "none",
-    },
     opacity: 0,
     pointerEvents: "none",
     transition: {
       y: { stiffness: 1000 },
     },
+    transitionEnd: {
+      display: "none",
+    },
   },
 };
 
 const Header = () => {
-  const [menu, setMenu] = useState(false);
+  const [menu, setMenu] = useState<boolean | undefined>(true);
 
   const isMd = useMediaQuery(EScreenType.md);
+  const mounted = useMounted();
 
   const toggleMenu = () => setMenu(!menu);
+
+  useEffect(() => {
+    setMenu(isMd ? true : undefined);
+  }, [isMd]);
 
   return (
     <header
@@ -67,6 +73,7 @@ const Header = () => {
         <motion.div
           whileHover={{ scale: 1.2 }}
           whileTap={{ scale: 0.9 }}
+          className="focus-visible: outline-none"
         >
           <MdMenuOpen
             className={clsxm("md:hidden block h-10 w-10 cursor-pointer", {
@@ -76,33 +83,43 @@ const Header = () => {
           />
         </motion.div>
       </motion.div>
-      <motion.nav
-        className={clsxm(
-          "md:gap-x-6 md:inline-flex gap-y-2 text-center flex-col md:flex-row",
-          {
-            ["mx-auto"]: menu,
-          }
-        )}
-        initial={{ display: "none" }}
-        animate={!isMd ? (menu ? "open" : "closed") : undefined}
-        {...(isMd && {
-          initial: { x: 500, opacity: 0.5, display: "flex" },
-          animate: { x: 0, opacity: 1 },
-          transition: { duration: 1, ease: "easeInOut" },
-        })}
-        variants={variants}
-      >
-        {NAVBAR_OPTIONS.map((item, index) => (
-          <Link
-            key={index}
-            href="#"
-            className="relative px-2 py-1.5 rounded-md cursor-pointer hover:scale-125 ease-in-out duration-300 font-medium tracking-wide group"
+      {mounted && (
+        <AnimatePresence
+          key={isMd.toString()}
+          initial={true}
+        >
+          <motion.nav
+            className={clsxm(
+              "md:gap-x-6 md:inline-flex gap-y-2 text-center hidden flex-col md:flex-row"
+            )}
+            initial={{ display: "none" }}
+            animate={
+              !isMd && typeof menu !== "undefined"
+                ? menu
+                  ? "open"
+                  : "closed"
+                : undefined
+            }
+            {...(isMd && {
+              initial: { x: 500, opacity: 0.5, display: "flex" },
+              animate: { x: 0, opacity: 1 },
+              transition: { duration: 1, ease: "easeInOut" },
+            })}
+            variants={variants}
           >
-            <span>{item.label}</span>
-            <span className="absolute -bottom-1 left-0 w-0 transition-all h-1 bg-indigo-400 group-hover:w-full" />
-          </Link>
-        ))}
-      </motion.nav>
+            {NAVBAR_OPTIONS.map((item, index) => (
+              <Link
+                key={index}
+                href="#"
+                className="relative px-2 py-1.5 rounded-md cursor-pointer hover:scale-125 ease-in-out duration-300 font-medium tracking-wide group"
+              >
+                <span>{item.label}</span>
+                <span className="absolute -bottom-1 left-0 w-0 transition-all h-1 bg-indigo-400 group-hover:w-full" />
+              </Link>
+            ))}
+          </motion.nav>
+        </AnimatePresence>
+      )}
     </header>
   );
 };
