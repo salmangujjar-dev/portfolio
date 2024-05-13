7336ae7b7a60d1fca237a042d4fdbe9b44b1af19
Salman Ahmed <isalmandev@gmail.com>
2024-05-13 06:24:11 +0500

    debug: 4

--- src/utils/mailService.ts
+++ src/utils/mailService.ts
@@ -4,9 +4,8 @@ require("dotenv").config();
 export async function sendMail(subject: string, email: string, body: string) {
   var transporter = nodemailer.createTransport({
     port: 465,
-    service: "smtp.gmail.com",
+    service: "gmail",
     auth: {
-      type: "login",
       user: process.env.NODEMAILER_EMAIL,
       pass: process.env.NODEMAILER_PW,
     },
